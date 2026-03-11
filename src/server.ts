75a7a406b15b0a4a733b23504efb659f5418b2fb
HFsa-RaShid <hafsa20cse028@gmail.com>
2026-03-11 14:34:40 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -44,9 +44,9 @@ const startServer = async () => {
 
     await connectDB(); 
     
-    app.listen(PORT, () => {
-      console.log(`🚀 Server is running on http://localhost:${PORT}`);
-    });
+    // app.listen(PORT, () => {
+    //   console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    // });
   } catch (error) {
     console.error("❌ Failed to start server:", error);
     process.exit(1);
